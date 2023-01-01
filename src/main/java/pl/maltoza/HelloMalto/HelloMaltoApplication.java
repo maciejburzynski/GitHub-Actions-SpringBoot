15251339050aac9232a49644a7b5765a8974b7a4
Maciej Burzyński <maciej.kollaps@gmail.com>
2023-01-01 13:29:26 +0100

    Add ininial classes

---
+++ src/main/java/pl/maltoza/HelloMalto/HelloMaltoApplication.java
@@ -0,0 +1,14 @@
+package pl.maltoza.HelloMalto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMaltoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloMaltoApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
