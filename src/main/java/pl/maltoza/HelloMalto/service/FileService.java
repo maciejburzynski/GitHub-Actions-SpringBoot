d59864653d1e25c42929def032f4289206faee07
Maciej Burzyński <maciej.kollaps@gmail.com>
2023-03-19 19:47:25 +0100

    Add attachment's content to sent mail

--- src/main/java/pl/maltoza/HelloMalto/service/FileService.java
+++ src/main/java/pl/maltoza/HelloMalto/service/FileService.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Order;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 @Service
@@ -15,6 +17,15 @@ public class FileService {
     public File createFile() throws IOException {
         File file = new File("src/main/resources/test.txt");
         file.createNewFile();
+        file.setWritable(true);
+
+        FileWriter fw = new FileWriter("src/main/resources/test.txt", true);
+        BufferedWriter bw = new BufferedWriter(fw);
+
+        bw.write("Hello World from HelloMalto");
+        bw.newLine();
+        bw.close();
+
         return file;
 
     }
